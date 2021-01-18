f57a0dd89f317d1c388e6de9cb70f9b30512d7cd
Leonhard S <leonhard-sei@outlook.com>
2021-01-18 09:33:18 +0100

    Use unittest.mock for offline tests

--- tests/listener_test.py
+++ tests/listener_test.py
@@ -1,26 +1,45 @@
 import asyncio
+import datetime
 import unittest
-from typing import cast
+from typing import Any, cast
 from unittest import mock
 
 import asyncpg
-from mock.database import MockConnection, MockPool  # type: ignore
 
-from apl_listener import EventListener  # pylint: disable=import-error
+import apl_listener  # pylint: disable=import-error
 
 
-class Test(unittest.IsolatedAsyncioTestCase):
+class TestOffline(unittest.IsolatedAsyncioTestCase):
+    """Test that can be performed without a database connection."""
 
-    async def test_test(self) -> None:
-        # Mock objects
-        pool = MockPool()
+    def setUp(self) -> None:
+        """Run before every individual test case."""
+        self._old_facility_converter = apl_listener._client._base_from_facility
 
-        listener = EventListener('s:example', cast(asyncpg.pool.Pool, pool))
+        async def dummy(*args: Any) -> int:
+            return 0
 
+        apl_listener._client._base_from_facility = dummy
+
+    def tearDown(self) -> None:
+        """Run after every individual test case."""
+        apl_listener._client._base_from_facility = self._old_facility_converter
+
+    async def test_connect(self) -> None:
+        """Test the event client instantiation and connection."""
+        TIMEOUT = 5.0
+        INTERVAL = 0.1
+        pool = cast(asyncpg.pool.Pool, mock.MagicMock())
+        listener = apl_listener.EventListener('s:example', pool)
         asyncio.get_running_loop().create_task(listener.connect())
-        await asyncio.sleep(5)
-
-        pool.raise_fk_violations = True
-        with self.assertRaises(asyncpg.exceptions.ForeignKeyViolationError):
-            await asyncio.sleep(1)
+        for _ in range(round(TIMEOUT/INTERVAL)):
+            if listener._dispatch_cache:
+                break
+            await asyncio.sleep(INTERVAL)
+        else:
+            self.fail(f'No message received after {TIMEOUT} seconds')
         await listener.close()
+        self.assertTrue(len(listener._dispatch_cache) >= 1)
+        last_update = listener._dispatch_last_update
+        time_since = last_update - datetime.datetime.now()
+        self.assertLess(time_since.total_seconds(), TIMEOUT)
